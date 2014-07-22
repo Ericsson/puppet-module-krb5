5311dff6c5cfd3851d09f0f057041b1da427eac5
Phil Friderici <phil.friderici@ericsson.com>
2014-07-22 15:07:20 +0000

    add spec tests for optional parameters

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -148,4 +148,307 @@ describe 'krb5' do
     it { should contain_file('krb5conf').with_content("#Managed by puppet, any changes will be overwritten\n\n\[logging\]\nadmin_server = FILE:\/var\/log\/kerberos_admin.log\n") }
   end
 
+  context 'with default_realm parameter set to <EXAMPLE.COM> and disabled logging defaults' do
+    let(:params) do
+      {
+        :logging_default      => '',
+        :logging_kdc          => '',
+        :logging_admin_server => '',
+        :default_realm        => 'EXAMPLE.COM',
+      }
+    end
+
+    it { should contain_class('krb5') }
+    it { should contain_package('krb5-libs') }
+    it { should contain_file('krb5conf').with({
+      'path'   => '/etc/krb5.conf',
+      'ensure' => 'present',
+      'owner'  => 'root',
+      'group'  => 'root',
+      'mode'   => '0644',
+    }) }
+
+    it { should contain_file('krb5conf').with_content("#Managed by puppet, any changes will be overwritten\n\n\[libdefaults\]\ndefault_realm = EXAMPLE.COM\n") }
+  end
+
+  context 'with default_realm parameter set to <EXAMPLE.COM> and disabled logging defaults' do
+    let(:params) do
+      {
+        :logging_default      => '',
+        :logging_kdc          => '',
+        :logging_admin_server => '',
+        :default_realm        => 'EXAMPLE.COM',
+      }
+    end
+
+    it { should contain_class('krb5') }
+    it { should contain_package('krb5-libs') }
+    it { should contain_file('krb5conf').with({
+      'path'   => '/etc/krb5.conf',
+      'ensure' => 'present',
+      'owner'  => 'root',
+      'group'  => 'root',
+      'mode'   => '0644',
+    }) }
+
+    it { should contain_file('krb5conf').with_content("#Managed by puppet, any changes will be overwritten\n\n\[libdefaults\]\ndefault_realm = EXAMPLE.COM\n") }
+  end
+
+  context 'with dns_lookup_realm parameter set to <true> and disabled logging defaults' do
+    let(:params) do
+      {
+        :logging_default      => '',
+        :logging_kdc          => '',
+        :logging_admin_server => '',
+        :dns_lookup_realm     => 'true',
+      }
+    end
+
+    it { should contain_class('krb5') }
+    it { should contain_package('krb5-libs') }
+    it { should contain_file('krb5conf').with({
+      'path'   => '/etc/krb5.conf',
+      'ensure' => 'present',
+      'owner'  => 'root',
+      'group'  => 'root',
+      'mode'   => '0644',
+    }) }
+
+    it { should contain_file('krb5conf').with_content("#Managed by puppet, any changes will be overwritten\n\n\[libdefaults\]\ndns_lookup_realm = true\n") }
+  end
+
+  context 'with dns_lookup_kdc parameter set to <true> and disabled logging defaults' do
+    let(:params) do
+      {
+        :logging_default      => '',
+        :logging_kdc          => '',
+        :logging_admin_server => '',
+        :dns_lookup_kdc       => 'true',
+      }
+    end
+
+    it { should contain_class('krb5') }
+    it { should contain_package('krb5-libs') }
+    it { should contain_file('krb5conf').with({
+      'path'   => '/etc/krb5.conf',
+      'ensure' => 'present',
+      'owner'  => 'root',
+      'group'  => 'root',
+      'mode'   => '0644',
+    }) }
+
+    it { should contain_file('krb5conf').with_content("#Managed by puppet, any changes will be overwritten\n\n\[libdefaults\]\ndns_lookup_kdc = true\n") }
+  end
+
+  context 'with ticket_lifetime parameter set to <24200> and disabled logging defaults' do
+    let(:params) do
+      {
+        :logging_default      => '',
+        :logging_kdc          => '',
+        :logging_admin_server => '',
+        :ticket_lifetime      => '24200',
+      }
+    end
+
+    it { should contain_class('krb5') }
+    it { should contain_package('krb5-libs') }
+    it { should contain_file('krb5conf').with({
+      'path'   => '/etc/krb5.conf',
+      'ensure' => 'present',
+      'owner'  => 'root',
+      'group'  => 'root',
+      'mode'   => '0644',
+    }) }
+
+    it { should contain_file('krb5conf').with_content("#Managed by puppet, any changes will be overwritten\n\n\[libdefaults\]\nticket_lifetime = 24200\n") }
+  end
+
+  context 'with default_keytab_name parameter set to </etc/host.keytab> and disabled logging defaults' do
+    let(:params) do
+      {
+        :logging_default      => '',
+        :logging_kdc          => '',
+        :logging_admin_server => '',
+        :default_keytab_name  => '/etc/host.keytab',
+      }
+    end
+
+    it { should contain_class('krb5') }
+    it { should contain_package('krb5-libs') }
+    it { should contain_file('krb5conf').with({
+      'path'   => '/etc/krb5.conf',
+      'ensure' => 'present',
+      'owner'  => 'root',
+      'group'  => 'root',
+      'mode'   => '0644',
+    }) }
+
+    it { should contain_file('krb5conf').with_content("#Managed by puppet, any changes will be overwritten\n\n\[libdefaults\]\ndefault_keytab_name = /etc/host.keytab\n") }
+  end
+
+  context 'with forwardable parameter set to <false> and disabled logging defaults' do
+    let(:params) do
+      {
+        :logging_default      => '',
+        :logging_kdc          => '',
+        :logging_admin_server => '',
+        :forwardable          => 'false',
+      }
+    end
+
+    it { should contain_class('krb5') }
+    it { should contain_package('krb5-libs') }
+    it { should contain_file('krb5conf').with({
+      'path'   => '/etc/krb5.conf',
+      'ensure' => 'present',
+      'owner'  => 'root',
+      'group'  => 'root',
+      'mode'   => '0644',
+    }) }
+
+    it { should contain_file('krb5conf').with_content("#Managed by puppet, any changes will be overwritten\n\n\[libdefaults\]\nforwardable = false\n") }
+  end
+
+  context 'with realms parameter set to valid <false> and disabled logging defaults' do
+    let(:params) do
+      {
+        :logging_default      => '',
+        :logging_kdc          => '',
+        :logging_admin_server => '',
+        :proxiable            => 'false',
+      }
+    end
+
+    it { should contain_class('krb5') }
+    it { should contain_package('krb5-libs') }
+    it { should contain_file('krb5conf').with({
+      'path'   => '/etc/krb5.conf',
+      'ensure' => 'present',
+      'owner'  => 'root',
+      'group'  => 'root',
+      'mode'   => '0644',
+    }) }
+
+    it { should contain_file('krb5conf').with_content("#Managed by puppet, any changes will be overwritten\n\n\[libdefaults\]\nproxiable = false\n") }
+  end
+
+  context 'with realms parameter set to valid hash and disabled logging defaults' do
+    let(:params) do
+      {
+        :logging_default      => '',
+        :logging_kdc          => '',
+        :logging_admin_server => '',
+        :realms               => {
+          'EXAMPLE.COM'       => {
+            'default_domain'  => 'example.com',
+            'kdc'             => [ 'kdc1.example.com:88', 'kdc2.example.com:88', ],
+            'admin_server'    => [ 'kdc1.example.com:749', 'kdc2.example.com:749', ],
+          },
+        },
+      }
+    end
+
+    it { should contain_class('krb5') }
+    it { should contain_package('krb5-libs') }
+    it { should contain_file('krb5conf').with({
+      'path'   => '/etc/krb5.conf',
+      'ensure' => 'present',
+      'owner'  => 'root',
+      'group'  => 'root',
+      'mode'   => '0644',
+    }) }
+
+
+    krb5conf_fixture = File.read(fixtures("krb5.conf.realms"))
+    it { should contain_file('krb5conf').with_content(krb5conf_fixture) }
+  end
+
+  context 'with appdefaults parameter set to valid hash and disabled logging defaults' do
+    let(:params) do
+      {
+        :logging_default      => '',
+        :logging_kdc          => '',
+        :logging_admin_server => '',
+        :appdefaults          => {
+          'pam' => {
+            'debug'           => 'false',
+            'ticket_lifetime' => '36000',
+            'renew_lifetime'  => '36000',
+            'forwardable'     => 'true',
+            'krb4_convert'    => 'false',
+          },
+        },
+      }
+    end
+
+    it { should contain_class('krb5') }
+    it { should contain_package('krb5-libs') }
+    it { should contain_file('krb5conf').with({
+      'path'   => '/etc/krb5.conf',
+      'ensure' => 'present',
+      'owner'  => 'root',
+      'group'  => 'root',
+      'mode'   => '0644',
+    }) }
+
+
+    krb5conf_fixture = File.read(fixtures("krb5.conf.appdefaults"))
+    it { should contain_file('krb5conf').with_content(krb5conf_fixture) }
+  end
+
+  context 'with domain_realm parameter set to <example.com => EXAMPLE.COM> and disabled logging defaults' do
+    let(:params) do
+      {
+        :logging_default      => '',
+        :logging_kdc          => '',
+        :logging_admin_server => '',
+        :domain_realm         => {
+          'example.com'       => 'EXAMPLE.COM',
+        }
+      }
+    end
+
+    it { should contain_class('krb5') }
+    it { should contain_package('krb5-libs') }
+    it { should contain_file('krb5conf').with({
+      'path'   => '/etc/krb5.conf',
+      'ensure' => 'present',
+      'owner'  => 'root',
+      'group'  => 'root',
+      'mode'   => '0644',
+    }) }
+
+    it { should contain_file('krb5conf').with_content("#Managed by puppet, any changes will be overwritten\n\n[domain_realm]\n.example.com = EXAMPLE.COM\nexample.com = EXAMPLE.COM\n") }
+  end
+
+  context 'with package parameter set to <krb5-test>' do
+    let(:params) do
+      {
+        :package => 'krb5-test',
+      }
+    end
+
+    it { should contain_class('krb5') }
+    it { should contain_package('krb5-test') }
+  end
+
+  context 'with krb5conf_file parameter set to </etc/opt/krb5.conf>' do
+    let(:params) do
+      {
+        :krb5conf_file => '/etc/opt/krb5.conf',
+      }
+    end
+
+    it { should contain_class('krb5') }
+    it { should contain_package('krb5-libs') }
+
+    it { should contain_file('krb5conf').with({
+      'path'   => '/etc/opt/krb5.conf',
+      'ensure' => 'present',
+      'owner'  => 'root',
+      'group'  => 'root',
+      'mode'   => '0644',
+    }) }
+  end
+
 end
