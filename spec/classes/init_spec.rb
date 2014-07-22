1fc617c66fee3d38140986fb94671c96396da758
Phil Friderici <phil.friderici@ericsson.com>
2014-07-22 15:00:38 +0000

    use fixtures

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,25 +1,27 @@
 require 'spec_helper'
 describe 'krb5' do
 
-  context 'with defaults for all parameters' do
+  context 'with default values for parameters' do
+
     it { should contain_class('krb5') }
     it { should contain_package('krb5-libs') }
     it { should contain_file('krb5conf').with({
+      'path'   => '/etc/krb5.conf',
       'ensure' => 'present',
       'owner'  => 'root',
       'group'  => 'root',
       'mode'   => '0644',
-      'content' => '#Managed by puppet, any changes will be overwritten
+    }) }
 
-[logging]
-default = FILE:/var/log/krb5libs.log
-kdc = FILE:/var/log/krb5kdc.log
-admin_server = FILE:/var/log/kadmind.log
-',})}
+    krb5conf_fixture = File.read(fixtures("krb5.conf.defaults"))
+    it { should contain_file('krb5conf').with_content(krb5conf_fixture) }
+
   end
+
   context 'with all parameters set' do
     let(:params) do
-      { :default_realm        => 'EXAMPLE.COM',
+      {
+        :default_realm        => 'EXAMPLE.COM',
         :dns_lookup_realm     => 'false',
         :dns_lookup_kdc       => 'false',
         :ticket_lifetime      => '24h',
@@ -47,45 +49,19 @@ admin_server = FILE:/var/log/kadmind.log
         }
       }
     end
+
+    it { should contain_class('krb5') }
+    it { should contain_package('krb5-libs') }
     it { should contain_file('krb5conf').with({
-      'content' => '#Managed by puppet, any changes will be overwritten
+      'path'   => '/etc/krb5.conf',
+      'ensure' => 'present',
+      'owner'  => 'root',
+      'group'  => 'root',
+      'mode'   => '0644',
+    }) }
 
-[logging]
-default = FILE:/var/log/krb5libs.log
-kdc = FILE:/var/log/krb5kdc.log
-admin_server = FILE:/var/log/kadmind.log
-
-[libdefaults]
-default_realm = EXAMPLE.COM
-dns_lookup_realm = false
-dns_lookup_kdc = false
-ticket_lifetime = 24h
-default_keytab_name = /etc/opt/quest/vas/host.keytab
-forwardable = true
-proxiable = true
-
-[appdefaults]
-pam = {
-         debug = false
-         forwardable = true
-         krb4_convert = false
-         renew_lifetime = 36000
-         ticket_lifetime = 36000
-}
-
-[realms]
-EXAMPLE.COM = {
-  kdc = kdc1.example.com:88
-  kdc = kdc2.example.com:88
-  admin_server = kdc1.example.com:749
-  admin_server = kdc2.example.com:749
-  default_domain = example.com
-}
-
-[domain_realm]
-.example.com = EXAMPLE.COM
-example.com = EXAMPLE.COM
-'}) }
+    krb5conf_fixture = File.read(fixtures("krb5.conf.allset"))
+    it { should contain_file('krb5conf').with_content(krb5conf_fixture) }
   end
 
   context 'with all logging parameters set to <> (overriding default values)' do
